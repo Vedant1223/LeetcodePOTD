f4eb77543445946a1d69f2204296278115094ebb
playeri <104373556+Vedant1223@users.noreply.github.com>
2025-05-24 19:54:09 +0530

    Time: 3 ms (26.74%) | Memory: 33 MB (80.27%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    vector<int> findWordsContaining(vector<string>& words, char x) {
-        vector<int> ans;
-        for(int i = 0 ; i < words.size(); i++){
-            for(auto c: words[i]){
-                if(c == x){
-                    ans.push_back(i);
-                    break;
-                }
+    std::vector<int> findWordsContaining(std::vector<std::string>& words, char x) {
+        std::vector<int> ans;
+        for (int i = 0; i < words.size(); i++) {
+            if (std::any_of(words[i].begin(), words[i].end(), [x](char ch) {
+                return ch == x;
+            })) {
+                ans.push_back(i);
             }
         }
-  
-    return ans;
+        return ans;
     }
 };
