801142a8a53d3522d1bde98e110cb654d60e8718
playeri <104373556+Vedant1223@users.noreply.github.com>
2025-05-25 12:43:34 +0530

    Time: 79 ms (59.05%) | Memory: 172.1 MB (20.37%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -5,27 +5,19 @@ public:
         int ans = 0 ;
         string str;
 
-        for(auto w : words){
-            str = "";
-            char a = w[0];
-            char b = w[1];
+        for (const string& w : words) {
+            string rev = {w[1], w[0]};
 
-            str+= b;
-            str +=a;
-
-            // cout<<str<<endl;
-            if(mp[str] == 0){
+            if (mp[rev] > 0) {
+                mp[rev]--;
+                ans += 4;
+            } else {
                 mp[w]++;
             }
-            else{
-                mp[str]--;
-                ans += 4;
-            }
         }
+
         for(char c = 'a'; c <= 'z'; c++){
-            str= "";
-            str+=c;
-            str+=c;
+            str = {c, c};
 
             if(mp[str] == 1){
                 ans +=2;
