b702c8ba153633c28c8055fb35ea62a88ab35963
playeri <104373556+Vedant1223@users.noreply.github.com>
2025-09-10 02:29:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (35.55%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    bool check( int n){
-        while(n > 0){
-            if(n % 10 == 0){
-                return false;
-            }
-            n  = n/10;
-        }
-        return true;
-    }
-
     vector<int> getNoZeroIntegers(int n) {
-        for( int i = 1; i <= n/2; i++){
-            if(check(i) && check(n-i)){
-                return {i, n-i};
+        int a = n;
+        int b = 0;
+        int take;
+        int unit = 1;
+
+        // we are splitting the n in 2 numbers and we will solve 
+        //ones then tens then hunderds then   thousands
+
+        while( n > 1){
+            take = 1;
+            if(n%10 == 1){
+                take = 2;
             }
+            a = a - take*unit;
+            b = b + take*unit;
+
+            n = (n-take)/10;
+
+            unit = unit * 10;
         }
-        return {};
+
+        return {a,b};
     }
 };
