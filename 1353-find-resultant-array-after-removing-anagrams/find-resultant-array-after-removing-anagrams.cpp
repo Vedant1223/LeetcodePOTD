25dc0e57aa4c82d7aa854e4235a36bc1d4c2ddc7
playeri <104373556+Vedant1223@users.noreply.github.com>
2025-10-13 23:35:54 +0530

    Time: 3 ms (48.84%) | Memory: 17.2 MB (29.19%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -1,29 +1,32 @@
-
 class Solution {
 public:
-    string countingSort(string str) {
-        int freq[26] = {0};
-        for (char c : str)
-            freq[c - 'a']++;
 
+    string countingSort( string str){
+        int freq[26] = {0};
+        for(char i : str){
+            int idx = i - 'a';
+            freq[idx]++;
+        }
         string ans;
-        for (int i = 0; i < 26; i++)
+
+        for(int i = 0 ; i < 26; i++){
             ans.append(freq[i], 'a' + i);
+        }
         return ans;
     }
 
     vector<string> removeAnagrams(vector<string>& words) {
         vector<string> result;
-        if (words.empty()) return result;
-
         result.push_back(words[0]);
-        string prevSorted = countingSort(words[0]);
+        string prevSort = countingSort(words[0]);
 
-        for (int i = 1; i < words.size(); i++) {
-            string currSorted = countingSort(words[i]);
-            if (currSorted != prevSorted) {
+        for( int i = 1; i < words.size(); i++){
+
+            string currSort = countingSort(words[i]);
+            
+            if(currSort != prevSort){
                 result.push_back(words[i]);
-                prevSorted = currSorted;
+                prevSort = currSort;
             }
         }
         return result;
